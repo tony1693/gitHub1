d56d34942590c6418c7fe1ea23c51ca0f539074e
tony1693 <toak_16@hotmail.com>
2023-11-03 20:46:33 +0100

    funciones

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function suma ( num1 , num2){
+    let num3 = 0;
+    console.log(num1 + num2);
+}
+suma;
+
+
+function resta (num1 , num2){
+    let num3 = 0;
+    console.log(num1 - num2);
+}
+resta(10 , 5);
+
+function multiplicar (num1 , num2){
+    let num3 = 0;
+    console.log(num1 * num2);
+}
+multiplicar(5 , 2);
+
+function division (num1 , num2){
+    let num3 = 0;
+    console.log(num1 / num2);
+}
+division(10,5);
+
+function numCuadrado (num1) {
+    console.log(num1 * num1);
+}
+numCuadrado(7);
+s
+module.exports = {suma,resta,multiplicar,division};
